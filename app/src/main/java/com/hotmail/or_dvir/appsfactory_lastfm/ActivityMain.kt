578b9fb90b8cefd7a67360cdf837b685a4fc5633
Or Dvir <or_dvir@hotmail.com>
2021-06-28 15:18:41 +0200

    added navigation graph and view bindings

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/ActivityMain.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/ActivityMain.kt
@@ -1,13 +1,26 @@
 package com.hotmail.or_dvir.appsfactory_lastfm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.hotmail.or_dvir.appsfactory_lastfm.databinding.ActivityMainBinding
 
 class ActivityMain : AppCompatActivity()
 {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        findNavController(R.id.nav_host_fragment).apply {
+            binding.toolbar.setupWithNavController(this, AppBarConfiguration(graph))
+        }
     }
 }
---
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/Frag.kt
@@ -0,0 +1,8 @@
+package com.hotmail.or_dvir.appsfactory_lastfm.vvm
+
+import androidx.fragment.app.Fragment
+
+class Frag: Fragment()
+{
+    //todo temporary. remove when not needed anymore
+}
