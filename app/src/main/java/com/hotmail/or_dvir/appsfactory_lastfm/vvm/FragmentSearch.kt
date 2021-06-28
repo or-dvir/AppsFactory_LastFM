6eabc8a2992df6bc598392800a796080cab746a5
Or Dvir <or_dvir@hotmail.com>
2021-06-28 23:15:19 +0200

    added basic search functionality to FragmentSearch

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentSearch.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentSearch.kt
@@ -11,8 +11,11 @@ import android.widget.EditText
 import androidx.annotation.VisibleForTesting
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
 import com.hotmail.or_dvir.appsfactory_lastfm.R
 import com.hotmail.or_dvir.appsfactory_lastfm.databinding.FragmentSearchBinding
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Artist
+import com.hotmail.or_dvir.appsfactory_lastfm.other.longSnackbar
 import com.hotmail.or_dvir.appsfactory_lastfm.vvm.base_classes.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -21,11 +24,21 @@ class FragmentSearch : BaseFragment()
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
+    //todo
+    // add recycler view with results of search
+    // add empty view
+
     @VisibleForTesting
     internal val viewModel: FragmentSearchViewModel by viewModel()
+    private lateinit var observerArtistsSearch: Observer<List<Artist>>
 
     override fun getLoadingView() = binding.loadingView.parent
     override fun getViewModel() = viewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initializeObservers()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +48,12 @@ class FragmentSearch : BaseFragment()
     {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.artists.observe(viewLifecycleOwner, observerArtistsSearch)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater)
@@ -57,22 +76,39 @@ class FragmentSearch : BaseFragment()
                 setHintTextColor(textColor)
             }
 
-            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?): Boolean
+            setOnQueryTextListener(
+                object : SearchView.OnQueryTextListener
                 {
-                    //todo perform actual search
-                    return true
-                }
+                    override fun onQueryTextSubmit(query: String?): Boolean
+                    {
+                        test me!!!!
+                        right now i get "malformed json"
+                        query?.let { viewModel.searchArtists(it) }
+                        return true
+                    }
 
-                override fun onQueryTextChange(newText: String?): Boolean
-                {
-                    //do nothing (but use default search view behaviour).
-                    return false
+                    override fun onQueryTextChange(newText: String?): Boolean
+                    {
+                        //do nothing (but use default search view behaviour).
+                        return false
+                    }
                 }
-            })
-
+            )
         }
     }
+
+    private fun initializeObservers() {
+        observerArtistsSearch = Observer {
+            if (it == null) {
+                view?.longSnackbar(R.string.error_general)
+                return@Observer
+            }
+
+            //todo add results to the recycler view
+            // use DiffUtil
+        }
+    }
+
 
     override fun onDestroyView()
     {
--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentSearchViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentSearchViewModel.kt
@@ -1,9 +1,27 @@
 package com.hotmail.or_dvir.appsfactory_lastfm.vvm
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Artist
+import com.hotmail.or_dvir.appsfactory_lastfm.other.repositories.RepositoryArtists
 import com.hotmail.or_dvir.appsfactory_lastfm.vvm.base_classes.BaseAndroidViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class FragmentSearchViewModel(app: Application) : BaseAndroidViewModel(app)
+class FragmentSearchViewModel(
+    app: Application,
+    private val repoArtists: RepositoryArtists
+) : BaseAndroidViewModel(app)
 {
+    val artists = MutableLiveData<List<Artist>>()
 
+    fun searchArtists(query: String)
+    {
+        viewModelScope.launch(Dispatchers.Main) {
+            isLoading.value = true
+            artists.value = repoArtists.searchArtists(query)
+            isLoading.value = false
+        }
+    }
 }
