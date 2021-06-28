45cc1c64709bfa5b30384dd983523f6f2fe96dbb
Or Dvir <or_dvir@hotmail.com>
2021-06-28 15:52:13 +0200

    Create ExtensionFunctions.kt

---
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/ExtensionFunctions.kt
@@ -0,0 +1,11 @@
+package com.hotmail.or_dvir.appsfactory_lastfm.other
+
+import android.view.View
+import androidx.annotation.StringRes
+import com.google.android.material.snackbar.Snackbar
+
+fun View.snackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
+fun View.snackbar(@StringRes textRes: Int) = snackbar(resources.getString(textRes))
+
+fun View.longSnackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_LONG).show()
+fun View.longSnackbar(@StringRes textRes: Int) = longSnackbar(resources.getString(textRes))
