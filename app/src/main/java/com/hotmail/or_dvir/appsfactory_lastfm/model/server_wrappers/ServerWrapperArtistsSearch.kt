70986277aba7dde2b25bf3750f07d6264aa941c2
Or Dvir <or_dvir@hotmail.com>
2021-06-30 12:53:49 +0200

    Create ServerWrapperArtistsSearch.kt

---
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/model/server_wrappers/ServerWrapperArtistsSearch.kt
@@ -0,0 +1,26 @@
+package com.hotmail.or_dvir.appsfactory_lastfm.model.server_wrappers
+
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Artist
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+class ServerWrapperArtistsSearch(
+    @Json(name = "results")
+    val result: Result
+)
+{
+    fun getArtistsSearchResults() = result.matches.artists
+}
+
+@JsonClass(generateAdapter = true)
+class Result(
+    @Json(name = "artistmatches")
+    val matches: ArtistMatches
+)
+
+@JsonClass(generateAdapter = true)
+class ArtistMatches(
+    @Json(name = "artist")
+    val artists: List<Artist>
+)
