0f0a433eb37d46c95f7a72e24533215d61481455
Or Dvir <or_dvir@hotmail.com>
2021-07-01 20:21:20 +0200

    Create ServerWrapperTopAlbums.kt

---
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/model/server_wrappers/ServerWrapperTopAlbums.kt
@@ -0,0 +1,20 @@
+package com.hotmail.or_dvir.appsfactory_lastfm.model.server_wrappers
+
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Album
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+class ServerWrapperTopAlbums(
+    @Json(name = "topalbums")
+    val topAlbums: TopAlbums
+)
+{
+    fun getTopAlbums() = topAlbums.albums
+}
+
+@JsonClass(generateAdapter = true)
+class TopAlbums(
+    @Json(name = "album")
+    val albums: List<Album>
+)
