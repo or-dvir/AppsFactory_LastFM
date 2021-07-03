f15a296a0e3f4f3557b4448728a2ce95b7a90935
Or Dvir <or_dvir@hotmail.com>
2021-07-03 20:10:22 +0200

    almost finished with albums fragment (missing adapter)

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentTopAlbums.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentTopAlbums.kt
@@ -1,34 +1,27 @@
 package com.hotmail.or_dvir.appsfactory_lastfm.vvm
 
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.annotation.VisibleForTesting
-import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.hotmail.or_dvir.appsfactory_lastfm.R
-import com.hotmail.or_dvir.appsfactory_lastfm.databinding.FragmentSearchBinding
-import com.hotmail.or_dvir.appsfactory_lastfm.model.Artist
+import com.hotmail.or_dvir.appsfactory_lastfm.databinding.FragmentTopAlbumsBinding
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Album
 import com.hotmail.or_dvir.appsfactory_lastfm.other.longSnackbar
 import com.hotmail.or_dvir.appsfactory_lastfm.other.snackbar
-import com.hotmail.or_dvir.appsfactory_lastfm.vvm.adapters.AdapterArtists
 import com.hotmail.or_dvir.appsfactory_lastfm.vvm.base_classes.BaseFragment
-import com.hotmail.or_dvir.dxclick.DxFeatureClick
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import or_dvir.hotmail.com.dxutils.hideKeyBoard
 import or_dvir.hotmail.com.dxutils.makeGone
 import or_dvir.hotmail.com.dxutils.makeVisibleOrGone
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -47,13 +40,18 @@ class FragmentTopAlbums : BaseFragment()
     // observe albums error
     // check api!!!! album object does not include list of tracks!!!
 
-    private var _binding: FragmentTopAlbums? = null
+    //todo
+    // update title (from arguments)
+
+    private var _binding: FragmentTopAlbumsBinding? = null
     private val binding get() = _binding!!
+    private val fragArgs: FragmentTopAlbumsArgs by navArgs()
 
     @VisibleForTesting
     internal val viewModel: FragmentTopAlbumsViewModel by viewModel()
-    private lateinit var observerArtistsSearch: Observer<List<Artist>>
-    private lateinit var rvAdapter: AdapterArtists
+    private lateinit var observerAlbums: Observer<List<Album>>
+    private lateinit var observerError: Observer<String>
+    private lateinit var rvAdapter: AdapterAlbums
 
     override fun getLoadingView() = binding.loadingView.parent
     override fun getViewModel() = viewModel
@@ -70,7 +68,7 @@ class FragmentTopAlbums : BaseFragment()
         savedInstanceState: Bundle?
     ): View
     {
-        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        _binding = FragmentTopAlbumsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -79,95 +77,48 @@ class FragmentTopAlbums : BaseFragment()
         super.onViewCreated(view, savedInstanceState)
 
         //initialize with empty list
-        rvAdapter = AdapterArtists(mutableListOf()).apply {
-            addFeature(
-                DxFeatureClick<Artist>(
-                    onItemClick = { view, adapterPosition, item ->
-                        //todo do me!!!
-                        getView()?.snackbar("click")
-                    },
-                    onItemLongClick = { view, adapterPosition, item ->
-                        //do nothing
-                        false
-                    }
-                )
-            )
-        }
+        rvAdapter = AdapterAlbums(mutableListOf())
 
-        binding.rv.apply {
-            //todo might be needed for pagination?
+        binding.apply {
+            tvTitle.text = getString(R.string.title_topAlbumsFor_s, fragArgs.artistName)
+
+            rv.apply {
+                //todo might be needed for pagination?
 //            onScrollListener = DxScrollListener(25).apply {
 //                onScrollDown = { getFab().hide() }
 //                onScrollUp = { getFab().show() }
 //            }
 
-            adapter = rvAdapter
+                adapter = rvAdapter
 
-            layoutManager =
-                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-
-            addItemDecoration(
-                DividerItemDecoration(
-                    context, (layoutManager as LinearLayoutManager).orientation
-                )
-            )
+                //todo dynamically calculate number of columns?
+                layoutManager = GridLayoutManager(context, 3)
+            }
         }
 
-        viewModel.artists.observe(viewLifecycleOwner, observerArtistsSearch)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater)
-    {
-        inflater.inflate(R.menu.menu_search, menu)
-        val searchView = menu.findItem(R.id.menuItem_search).actionView as SearchView
-        setupSearchView(searchView)
-
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    private fun setupSearchView(sv: SearchView)
-    {
-        sv.apply {
-            queryHint = getString(R.string.hint_artistName)
-            isSubmitButtonEnabled = true
-            findViewById<EditText>(androidx.appcompat.R.id.search_src_text).apply {
-                setTextColor(getColor(R.color.white))
-                setHintTextColor(getColor(R.color.whiteTransparent))
-            }
-
-            setOnQueryTextListener(
-                object : SearchView.OnQueryTextListener
-                {
-                    override fun onQueryTextSubmit(query: String?): Boolean
-                    {
-                        query?.let { viewModel.searchArtists(it) }
-                        return true
-                    }
-
-                    override fun onQueryTextChange(newText: String?): Boolean
-                    {
-                        //do nothing (but use default search view behaviour).
-                        return false
-                    }
-                }
-            )
+        viewModel.apply {
+            albums.observe(viewLifecycleOwner, observerAlbums)
+            //todo test what happens when there is an error, then the user goes to the home screen,
+            // then returns to the app - does the error message show again?
+            error.observe(viewLifecycleOwner, observerError)
         }
     }
 
     private fun initializeObservers()
     {
-        observerArtistsSearch = Observer { newList ->
-            view?.let { hideKeyBoard(it, InputMethodManager.HIDE_NOT_ALWAYS) }
+        observerError = Observer {
+            view?.snackbar(it ?: getString(R.string.error_general))
+        }
 
+        observerAlbums = Observer { newList ->
             if (newList == null)
             {
                 binding.apply {
                     rv.makeGone()
-                    emptyView.makeGone()
+                    //todo make better error
+                    tvTitle.text = getString(R.string.error_general)
                 }
 
-                //todo make better error
-                view?.longSnackbar(R.string.error_general)
                 return@Observer
             }
 
@@ -177,14 +128,15 @@ class FragmentTopAlbums : BaseFragment()
                 view?.longSnackbar(R.string.error_general)
             }
 
+            //todo copied from search fragment. can i make this shared?
             //ui operation - should by tied to lifecycle scope.
             //uses main dispatcher by default.
             lifecycleScope.launch(exceptionHandler) {
                 //switch to Dispatchers.default which is recommended for DiffUtil
                 val result = withContext(Dispatchers.Default) {
                     DiffUtil.calculateDiff(
-                        Artist.DiffCallback(rvAdapter.items, newList),
-                        true
+                        Album.DiffCallback(rvAdapter.items, newList),
+                        false
                     )
                 }
 
@@ -195,22 +147,13 @@ class FragmentTopAlbums : BaseFragment()
 
                     //handle views visibility
                     binding.apply {
-                        //if no results were found, and the user has performed at least one
-                        // search before, show an appropriate error message.
-                        //(this observer will be called with an empty list the first time the
-                        // fragment is created)
-                        if (isEmpty() && viewModel.hasSearchedBefore)
+                        if (isEmpty())
                         {
-                            emptyView.text =
-                                String.format(
-                                    getString(R.string.error_artistSearchResults_s),
-                                    viewModel.lastSearchQuery
-                                )
+                            tvTitle.text =
+                                getString(R.string.error_noResults_s, fragArgs.artistName)
                         }
 
                         rv.makeVisibleOrGone(!isEmpty())
-                        emptyView.makeVisibleOrGone(isEmpty())
-                        rv.scrollToPosition(0)
                     }
                 }
             }
