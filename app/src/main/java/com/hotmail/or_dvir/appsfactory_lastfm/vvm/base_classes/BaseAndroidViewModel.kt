89f46069aa56c960dcaba7f35c855ad4355541f5
Or Dvir <or_dvir@hotmail.com>
2021-07-03 18:24:47 +0200

    added convenience methods to BaseFragment

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/base_classes/BaseAndroidViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/base_classes/BaseAndroidViewModel.kt
@@ -1,11 +1,17 @@
 package com.hotmail.or_dvir.appsfactory_lastfm.vvm.base_classes
 
 import android.app.Application
+import android.support.annotation.StringRes
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import org.koin.core.component.KoinComponent
 
-abstract class BaseAndroidViewModel(app: Application) : AndroidViewModel(app), KoinComponent
+abstract class BaseAndroidViewModel(val app: Application) : AndroidViewModel(app), KoinComponent
 {
     val isLoading = MutableLiveData<Boolean>()
+
+    fun getString(@StringRes stringRes: Int) = app.getString(stringRes)
+
+    fun getString(@StringRes stringRes: Int, vararg args: String) =
+        String.format(app.getString(stringRes), args)
 }
