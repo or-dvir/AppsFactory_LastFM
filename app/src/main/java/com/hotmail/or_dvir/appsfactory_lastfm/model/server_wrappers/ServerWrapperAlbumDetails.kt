13aefa5fa46fba5b904689510b267e87bd4e19a8
Or Dvir <or_dvir@hotmail.com>
2021-07-04 16:24:27 +0200

    updated server wrapper for album details
    originally i used the already existing Album object, which holds an Artist object (required for another endpoint of the API), but in this case the server only returns a string.
    the solution is to mimic the server wrapper as it is, and convert it to the "normal" Album object somewhere else in the code.
    this unfortunately means some code duplication

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/model/server_wrappers/ServerWrapperAlbumDetails.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/model/server_wrappers/ServerWrapperAlbumDetails.kt
@@ -1,11 +1,22 @@
 package com.hotmail.or_dvir.appsfactory_lastfm.model.server_wrappers
 
-import com.hotmail.or_dvir.appsfactory_lastfm.model.Album
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Image
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Tracks
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 class ServerWrapperAlbumDetails(
-    @Json(name = "album")
-    val album: Album
+    @Json(name = "name")
+    val albumName: String,
+    @Json(name = "artist")
+    val artistName: String,
+    @Json(name = "image")
+    val images: List<Image> = listOf(),
+    @Json(name = "tracks")
+    val tracks: Tracks?,
+    @Json(name = "mbid")
+    val id: String
 )
+//note: cannot use existing Album class because it requires an Artist object, whereas here
+// the artist is only a string. Conversion will have to be done elsewhere
--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentAlbumDetailsViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentAlbumDetailsViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.hotmail.or_dvir.appsfactory_lastfm.model.Album
+import com.hotmail.or_dvir.appsfactory_lastfm.model.Artist
+import com.hotmail.or_dvir.appsfactory_lastfm.model.server_wrappers.ServerWrapperAlbumDetails
 import com.hotmail.or_dvir.appsfactory_lastfm.other.repositories.RepositoryAlbums
 import com.hotmail.or_dvir.appsfactory_lastfm.vvm.base_classes.BaseAndroidViewModel
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +17,7 @@ class FragmentAlbumDetailsViewModel(
 ) : BaseAndroidViewModel(app)
 {
     //todo add option to add/remove album to/from favorites?
-    
+
     //do NOT initialize this with null or empty data! the UI observer will be triggered!
     //todo observe me!
     val album = MutableLiveData<Album>()
@@ -37,8 +39,19 @@ class FragmentAlbumDetailsViewModel(
     {
         viewModelScope.launch(Dispatchers.Main) {
             isLoading.value = true
-            album.value = repoAlbums.getAlbumDetails(artistName, albumName).album
+            val serverAlbumDetails = repoAlbums.getAlbumDetails(artistName, albumName)
+            album.value = convertServerAlbumToAppAlbum(serverAlbumDetails)
             isLoading.value = false
         }
     }
+
+    private fun convertServerAlbumToAppAlbum(serverAlbum: ServerWrapperAlbumDetails) =
+        Album(
+            serverAlbum.id,
+            serverAlbum.albumName,
+            //todo is empty id ok here? better idea?
+            Artist("", serverAlbum.artistName),
+            serverAlbum.images,
+            serverAlbum.tracks
+        )
 }
