7f8a4ac971eac7410ea50117004cf1ce370f50c1
Or Dvir <or_dvir@hotmail.com>
2021-07-08 23:44:25 +0200

    changed extensionfunction isBlankOrNull() to isBlankOrEquals()

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/ExtensionFunctions.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/ExtensionFunctions.kt
@@ -3,7 +3,6 @@ package com.hotmail.or_dvir.appsfactory_lastfm.other
 import android.view.View
 import androidx.annotation.StringRes
 import com.google.android.material.snackbar.Snackbar
-import or_dvir.hotmail.com.dxutils.toLowerCaseDefaultLocale
 
 fun View.snackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
 fun View.snackbar(@StringRes textRes: Int) = snackbar(resources.getString(textRes))
@@ -12,7 +11,6 @@ fun View.longSnackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_
 fun View.longSnackbar(@StringRes textRes: Int) = longSnackbar(resources.getString(textRes))
 
 /**
- * checks if this string is blank or equals to the string "null".
- * not to be confused with [isNullOrBlank]
+ * checks if this string is blank or equals to the given [str] (ignoring the case)
  */
-fun String.isBlankOrNull() = this.isBlank() || this.toLowerCaseDefaultLocale() == "null"
+fun String.isBlankOrEquals(str: String) = this.isBlank() || this.equals(str, true)
