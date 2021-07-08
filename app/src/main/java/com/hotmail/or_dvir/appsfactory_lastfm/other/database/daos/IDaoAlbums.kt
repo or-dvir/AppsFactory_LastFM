3144b42bbeb5d8f49fdaba366ee18a8317be44a5
Or Dvir <or_dvir@hotmail.com>
2021-07-08 22:38:47 +0200

    Update IDaoAlbums.kt

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/database/daos/IDaoAlbums.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/database/daos/IDaoAlbums.kt
@@ -1,15 +1,21 @@
 package com.hotmail.or_dvir.appsfactory_lastfm.other.database.daos
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.hotmail.or_dvir.appsfactory_lastfm.model.Album
 
 @Dao
 interface IDaoAlbums
 {
     /**
+     * note that this function would use the primary key of Album
+     * which is NOT a reliable id (see Album.dbUUID).
+     * for simplicity, we use this default method for insertion,
+     * but further checks need to be made before calling it or we may end up with
+     * bad data.
+     *
      * @return Long the id of the inserted row (NOT the object!), or -1 if failed
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -18,6 +24,6 @@ interface IDaoAlbums
     /**
      * @return Int number of rows deleted
      */
-    @Delete
-    suspend fun delete(album: Album): Int
+    @Query("delete from table_favoriteAlbums where dbUUID=:dbUUID")
+    suspend fun delete(dbUUID: String): Int
 }
