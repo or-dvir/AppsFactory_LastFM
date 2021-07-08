b84d58d6f888b2ee46ad42989649b648de21120c
Or Dvir <or_dvir@hotmail.com>
2021-07-08 21:07:29 +0200

    added todo notes

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/repositories/RepositoryAlbumsImpl.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/repositories/RepositoryAlbumsImpl.kt
@@ -14,11 +14,17 @@ class RepositoryAlbumsImpl(
 {
     //todo try all kinds of scenarios and handle errors appropriately
     //todo the api has a list of error codes. handle them!!!
+    //todo how do i keep the information in the database up to date?
+    //      if the downloaded album is in my favorites, also update it?
+    //      maybe for simplicity just say that the information is only updated
+    //      if the user removes and saves the information again
     override suspend fun getTopAlbums(artistName: String) =
         withContext(Dispatchers.IO) {
             lastFmApi.getTopAlbums(artistName)
         }
 
+    //todo this should work offline!
+    // if no internet connection, load from database
     override suspend fun getAlbumDetails(
         artistName: String,
         albumName: String
