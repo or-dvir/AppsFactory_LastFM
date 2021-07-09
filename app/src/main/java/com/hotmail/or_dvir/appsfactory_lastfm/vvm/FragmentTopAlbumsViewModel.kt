aeac2b73edb0b1b9e9d767300f5342fbe4897dbb
Or Dvir <or_dvir@hotmail.com>
2021-07-09 19:38:02 +0200

    added favorite albums to the view model so it can be send to the adapter when needed

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentTopAlbumsViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentTopAlbumsViewModel.kt
@@ -16,31 +16,36 @@ class FragmentTopAlbumsViewModel(
     private val repoAlbums: RepositoryAlbums
 ) : BaseAndroidViewModel(app)
 {
-    val albums = MutableLiveData<List<Album>?>(listOf())
+    val topAlbums = MutableLiveData<List<Album>?>(listOf())
 
     //do NOT initialize the error! if you do, the UI will respond when the view model is created
+    //todo scenario: say we had an error, fragment goes to background, fragment comes back
+    //      to foreground. live data will trigger and we will see the same error again!
     val error = MutableLiveData<String>()
-    var favoriteAlbums: List<Album>? = null
+    var favoriteAlbums = listOf<Album>()
 
     fun getTopAlbumsAndLoadFavorites(artistName: String?)
     {
         if (artistName == null)
         {
-            albums.value = null
+            topAlbums.value = null
             return
         }
 
         viewModelScope.launch(Dispatchers.Main) {
             isLoading.value = true
-            //load top albums and favorites in parallel
+            //load top albums and favorites in parallel.
+            //we load favorite albums here so we can pass it on later to the albums adapter
+            //so that we may display the correct "favorite" icon to the user.
+            //it is more efficient to query for the favorites here than in the adapter
             val favoriteDeferred = async { repoAlbums.getFavoriteAlbums() }
             val topDeferred = async { repoAlbums.getTopAlbums(artistName).getTopAlbums() }
 
+            //todo is this the best place to load favorite albums?
+            // maybe in the fragment before updating the adapter?
             favoriteAlbums = favoriteDeferred.await()
-            albums.value = topDeferred.await()
+            topAlbums.value = topDeferred.await()
 
-            i stopped here. now when top albums are in the recycler, favoriteAlbums field
-            should be set and can be passed to the adapter
             isLoading.value = false
         }
     }
@@ -49,29 +54,34 @@ class FragmentTopAlbumsViewModel(
     // when adding and removing albums to/from favorites, should i show a loading dialog
     //      on the album itself???
     fun addAlbumToFavorites(album: Album) = addOrRemoveAlbum(true, album)
-
     fun removeAlbumFromFavorites(album: Album) = addOrRemoveAlbum(false, album)
 
     private fun addOrRemoveAlbum(isAdding: Boolean, album: Album)
     {
         viewModelScope.launch(Dispatchers.Main) {
-            val success = if (isAdding)
-            {
-                repoAlbums.addFavoriteAlbum(album)
-            } else
-            {
-                repoAlbums.deleteFavoriteAlbum(album)
-            }
+            isLoading.value = true
+
+            val success =
+                if (isAdding)
+                {
+                    repoAlbums.addFavoriteAlbum(album)
+                } else
+                {
+                    repoAlbums.deleteFavoriteAlbum(album)
+                }
+
+            isLoading.value = false
 
             if (!success)
             {
-                val errorRes = if (isAdding)
-                {
-                    R.string.error_addingAlbum_s
-                } else
-                {
-                    R.string.error_removingAlbum_s
-                }
+                val errorRes =
+                    if (isAdding)
+                    {
+                        R.string.error_addingAlbum_s
+                    } else
+                    {
+                        R.string.error_removingAlbum_s
+                    }
 
                 error.value = getString(errorRes, album.name ?: "")
             }
