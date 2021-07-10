491b90e815e13360ea8732dcf5701c4f21b432b1
Or Dvir <or_dvir@hotmail.com>
2021-07-10 21:39:44 +0200

    removed list of favorite albums from top albums view model

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentTopAlbumsViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/vvm/FragmentTopAlbumsViewModel.kt
@@ -8,7 +8,6 @@ import com.hotmail.or_dvir.appsfactory_lastfm.model.Album
 import com.hotmail.or_dvir.appsfactory_lastfm.other.repositories.RepositoryAlbums
 import com.hotmail.or_dvir.appsfactory_lastfm.vvm.base_classes.BaseAndroidViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class FragmentTopAlbumsViewModel(
@@ -18,10 +17,7 @@ class FragmentTopAlbumsViewModel(
 {
     val topAlbums = MutableLiveData<List<Album>?>(listOf())
 
-    //todo should probably make this observe a livedata query from room
-    var favoriteAlbums = listOf<Album>()
-
-    fun getTopAlbumsAndLoadFavorites(artistName: String?)
+    fun loadTopAlbums(artistName: String?)
     {
         if (artistName == null)
         {
@@ -31,18 +27,7 @@ class FragmentTopAlbumsViewModel(
 
         viewModelScope.launch(Dispatchers.Main) {
             isLoading.value = true
-            //load top albums and favorites in parallel.
-            //we load favorite albums here so we can pass it on later to the albums adapter
-            //so that we may display the correct "favorite" icon to the user.
-            //it is more efficient to query for the favorites here than in the adapter
-            val favoriteDeferred = async { repoAlbums.getFavoriteAlbums() }
-            val topDeferred = async { repoAlbums.getTopAlbums(artistName).getTopAlbums() }
-
-            //todo is this the best place to load favorite albums?
-            // maybe in the fragment before updating the adapter?
-            favoriteAlbums = favoriteDeferred.await()
-            topAlbums.value = topDeferred.await()
-
+            topAlbums.value = repoAlbums.getTopAlbums(artistName).getTopAlbums()
             isLoading.value = false
         }
     }
@@ -61,17 +46,13 @@ class FragmentTopAlbumsViewModel(
             return
         }
 
-        //we do not use the contains() function because that is based on Album.equals().
-        //since Album is a data class, its' equals() function consists of all the fields in the
-        //primary constructor, whereas 2 albums might be the same even with slightly varied fields
-        //(e.g. from database and from lastFM API, since they don't exactly match).
-        val isInFavorite = favoriteAlbums.find { it.dbUUID == album.dbUUID } != null
-
         var success: Boolean
         var error: String? = null
         viewModelScope.launch(Dispatchers.Main) {
             isLoading.value = true
 
+            //todo do i have to observe this first to make it work?
+            val isInFavorite = repoAlbums.getFavoriteAlbums().value?.contains(album) ?: false
             if (isInFavorite)
             {
                 //removing album from favorites
