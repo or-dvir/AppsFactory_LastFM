dd0ee99295182e163c70089ba23f691b8e518228
Or Dvir <or_dvir@hotmail.com>
2021-07-15 23:09:52 +0200

    hiding keyboard when showing snackbar so its not hidden

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/ExtensionFunctions.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/ExtensionFunctions.kt
@@ -6,14 +6,31 @@ import android.net.NetworkInfo
 import android.view.View
 import androidx.annotation.StringRes
 import com.google.android.material.snackbar.Snackbar
+import or_dvir.hotmail.com.dxutils.hideKeyBoard
 
-//todo when done, delete unused functions
+fun View.snackbar(text: String)
+{
+    hideKeyBoard(this, 0)
+    Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
+}
 
-fun View.snackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
-fun View.snackbar(@StringRes textRes: Int) = snackbar(resources.getString(textRes))
+fun View.snackbar(@StringRes textRes: Int)
+{
+    hideKeyBoard(this, 0)
+    snackbar(resources.getString(textRes))
+}
 
-fun View.longSnackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_LONG).show()
-fun View.longSnackbar(@StringRes textRes: Int) = longSnackbar(resources.getString(textRes))
+fun View.longSnackbar(text: String)
+{
+    hideKeyBoard(this, 0)
+    Snackbar.make(this, text, Snackbar.LENGTH_LONG).show()
+}
+
+fun View.longSnackbar(@StringRes textRes: Int)
+{
+    hideKeyBoard(this, 0)
+    longSnackbar(resources.getString(textRes))
+}
 
 /**
  * checks if this string is blank or equals to the given [str] (ignoring the case)
