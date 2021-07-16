f8a50e767c933015da5adada4653431320e1eec0
Or Dvir <or_dvir@hotmail.com>
2021-07-16 21:56:48 +0200

    edited note

--- app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/retrofit/ILastFmApi.kt
+++ app/src/main/java/com/hotmail/or_dvir/appsfactory_lastfm/other/retrofit/ILastFmApi.kt
@@ -14,12 +14,10 @@ interface ILastFmApi
         private const val API_PARAMS = "api_key=$API_KEY&format=json"
     }
 
-    //todo add pagination for all operations!!!
-
     //instead of implementing paging, we limit the search query to 100 results.
-    //not only will this mean less code (and therefore potential bugs), but it also makes sense
-    //because if the user cannot find the artist they were looking for in the first 100 results,
-    //they probably need to adjust their search query.
+    //not only does this mean less code (and therefore less potential bugs),
+    //but it also makes sense. if the user cannot find the artist they were looking for
+    //in the first 100 results, they probably need to adjust their search query.
     //if we really wanted to display all the results, we would use google's Paging library
     @GET("?method=artist.search&$API_PARAMS&limit=100")
     suspend fun search(@Query("artist") searchQuery: String): ServerWrapperArtistsSearch
